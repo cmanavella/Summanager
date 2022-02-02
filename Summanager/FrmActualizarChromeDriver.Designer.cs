9eddc1dc81cb208e9f2a67be5a67a6e301dc02df
Cristian A. Manavella <crismanavella@gmail.com>
2022-02-02 11:25:48 -0300

    Hago el proceso para actualizar el chromedriver pero aun no es funcional.

---
+++ Summanager/FrmActualizarChromeDriver.Designer.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Summanager
+{
+    partial class FrmActualizarChromeDriver
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.FlatAppearance.BorderSize = 0;
+            // 
+            // worker
+            // 
+            this.worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.worker_DoWork);
+            this.worker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.worker_RunWorkerCompleted);
+            // 
+            // FrmActualizarChromeDriver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(233, 81);
+            this.Name = "FrmActualizarChromeDriver";
+            this.Shown += new System.EventHandler(this.FrmActualizarChromeDriver_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
