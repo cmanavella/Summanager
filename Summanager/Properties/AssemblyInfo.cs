ed4c273fa619d93e111068dc86eb83d8e92cbefc
Cristian Manavella <cris_manavella@hotmail.com>
2023-08-24 09:15:06 -0300

    Cambio la versión del sistema a 4.3.0

--- Summanager/Properties/AssemblyInfo.cs
+++ Summanager/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("SumManager")]
-[assembly: AssemblyDescription("Softrware para excanear el estado de suministros de impresoras conectadas en la Red.")]
+[assembly: AssemblyDescription("Softrware para escanear el estado de suministros de impresoras conectadas en la Red.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SumManager")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.2.1.0")]
-[assembly: AssemblyFileVersion("4.2.1.0")]
+[assembly: AssemblyVersion("4.3.0.0")]
+[assembly: AssemblyFileVersion("4.3.0.0")]
