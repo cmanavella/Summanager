dbeabe63f5a4c8aca1872a59ee343ef71d22c99b
Cristian Manavella <cris_manavella@hotmail.com>
2023-08-28 12:06:04 -0300

    Corrijo problema de toner no leído en Impresora 410.

--- IO/WebScraping.cs
+++ IO/WebScraping.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using System;
 using System.Collections.ObjectModel;
+using CustomExceptions;
 
 namespace IO
 {
@@ -66,7 +67,7 @@ namespace IO
             try
             {
                 //Accedo a la página que contiene la info de los suministros.
-                this.url += Printer.L410_AFTER_URL;
+                this.url += "hola"; //Printer.L410_AFTER_URL;
                 this.driver.Navigate().GoToUrl(this.url);
 
                 //Traigo el contenedor que contiene el toner. En este caso, es un elemento b dentro de un td
@@ -81,7 +82,15 @@ namespace IO
             }
             catch (Exception ex)
             {
-
+                //Si se ejecuta una excepción por StartIndex en uno de los suministros quiere decir que la impresora no lo está mostrando.
+                if (ex.Source == "mscorlib")
+                {
+                    throw new SuministroException();
+                }
+                else
+                {
+                    throw ex;
+                }
             }
         }
 
